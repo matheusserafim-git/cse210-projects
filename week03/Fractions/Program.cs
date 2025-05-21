e0b7716155ad4fdb9ea354b28aa27aa05f8f33b9
matheusserafim <matheussud8@gmail.com>
2025-05-21 16:40:29 -0300

    new

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fractions f1 = new Fractions(); //1/1
+        Fractions f2 = new Fractions(6);
+        Fractions f3 = new Fractions(6, 7);
+        Fractions f4 = new Fractions(3, 4);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        
+        //    Public void Fractions()
+        //    {
+        //        Fractions(wholeNumber: int)
+        //        {
+        //
+        //        }
+        //        Fractions(int _top, int _bottom)
+        //        {
+        //
+        //        }
+        //    }
+        //
+        //    Public void GetTop()
+        //    {
+        //        private SetTop(int _top)
+        //        {
+        //
+        //        }
+        //        private GetBottom()
+        //        {
+        //
+        //        }
+        //        private SetBotoom(int _bottom)
+        //        {
+        //
+        //        }
+        //
+        //    }
+        //
+        //    Public string GetfractionString(string)
+        //    {
+        //
+        //    }
+
+        //
+        //    
+        //    
+        //
+        //}
     }
 }
